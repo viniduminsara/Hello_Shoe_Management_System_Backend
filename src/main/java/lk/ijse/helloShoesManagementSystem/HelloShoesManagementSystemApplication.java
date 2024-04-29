a45d213b3784fb3c2ff96a74cf09cda10bab5d2f
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2024-04-29 06:11:29 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/helloShoesManagementSystem/HelloShoesManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.helloShoesManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloShoesManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloShoesManagementSystemApplication.class, args);
+	}
+
+}
