7676e910a288df8afbe4a021e960093f2f1429b5
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2024-05-02 13:03:34 +0530

    few changes

--- src/main/java/lk/ijse/helloShoesManagementSystem/HelloShoesManagementSystemApplication.java
+++ src/main/java/lk/ijse/helloShoesManagementSystem/HelloShoesManagementSystemApplication.java
@@ -1,10 +1,17 @@
 package lk.ijse.helloShoesManagementSystem;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HelloShoesManagementSystemApplication {
+
+	@Bean
+	ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloShoesManagementSystemApplication.class, args);
