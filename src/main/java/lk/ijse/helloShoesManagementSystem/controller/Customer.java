c2bf18f93894d667f8e010b3de41ac2c9d1f7252
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2024-05-02 19:31:05 +0530

    add logs

--- src/main/java/lk/ijse/helloShoesManagementSystem/controller/Customer.java
+++ src/main/java/lk/ijse/helloShoesManagementSystem/controller/Customer.java
@@ -4,6 +4,8 @@ import lk.ijse.helloShoesManagementSystem.dto.CustomerDTO;
 import lk.ijse.helloShoesManagementSystem.exception.NotFoundException;
 import lk.ijse.helloShoesManagementSystem.service.CustomerService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 public class Customer {
 
     private final CustomerService customerService;
+    private static final Logger logger = LoggerFactory.getLogger(Customer.class);
 
     @GetMapping("/health")
     public String customerHealth(){
@@ -30,42 +33,52 @@ public class Customer {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                     .body(bindingResult.getFieldErrors().get(0).getDefaultMessage());
         }
+        logger.info("Received request for save a customer");
         try {
             customerService.saveCustomer(customerDTO);
+            logger.info("Request processed successfully");
             return ResponseEntity.status(HttpStatus.CREATED).build();
         }catch (Exception e){
+            logger.error("An exception occurred: {}", e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> getAllCustomers(){
+        logger.info("Received request for get All customers");
         try {
             return ResponseEntity.ok(customerService.getAllCustomers());
         }catch (Exception e){
+            logger.error("An exception occurred: {}", e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<?> getSelectedCustomer(@PathVariable("id") String id){
+        logger.info("Received request for get a customer");
         try {
             return ResponseEntity.ok(customerService.getSelectedCustomer(id));
         }catch (NotFoundException e){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }catch (Exception e){
+            logger.error("An exception occurred: {}", e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
 
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<?> deleteCustomer(@PathVariable("id") String id){
+        logger.info("Received request for delete a customer");
         try {
             customerService.deleteCustomer(id);
+            logger.info("Request processed successfully");
             return ResponseEntity.status(HttpStatus.OK).build();
         }catch (NotFoundException e){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }catch (Exception e){
+            logger.error("An exception occurred: {}", e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
@@ -75,16 +88,19 @@ public class Customer {
                                             @Validated @RequestBody CustomerDTO customerDTO,
                                             BindingResult bindingResult)
     {
+        logger.info("Received request for update a customer");
         if (bindingResult.hasErrors()){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                     .body(bindingResult.getFieldErrors().get(0).getDefaultMessage());
         }
         try {
             customerService.updateCustomer(id, customerDTO);
+            logger.info("Request processed successfully");
             return ResponseEntity.status(HttpStatus.OK).build();
         }catch (NotFoundException e){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }catch (Exception e){
+            logger.error("An exception occurred: {}", e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
