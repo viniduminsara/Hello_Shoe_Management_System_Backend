692905b8d5a6752ef74a2303c43a93ae54dfb221
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2024-05-02 16:52:29 +0530

    feat: customer validation

--- src/main/java/lk/ijse/helloShoesManagementSystem/dto/CustomerDTO.java
+++ src/main/java/lk/ijse/helloShoesManagementSystem/dto/CustomerDTO.java
@@ -1,5 +1,6 @@
 package lk.ijse.helloShoesManagementSystem.dto;
 
+import jakarta.validation.constraints.*;
 import lk.ijse.helloShoesManagementSystem.entity.enums.Gender;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,13 +14,36 @@ import java.sql.Date;
 
 public class CustomerDTO {
 
+    @Null
     private String customerId;
+
+    @NotBlank(message = "Customer name cannot be blank")
     private String name;
+
+    @NotNull(message = "Gender cannot be null")
     private Gender gender;
+
+    @Null
+    @PastOrPresent(message = "Join date must be in the past or present")
     private Date joinedDate;
+
+    @Past(message = "Date of birth should be in the past")
+    @NotNull(message = "Date of birth cannot be null")
     private Date dob;
+
+    @NotBlank(message = "Address cannot be blank")
     private String address;
+
+    @NotBlank(message = "Contact No cannot be blank")
+    @Pattern(regexp = "^\\+?[0-9\\-\\s]+$", message = "Invalid contact number format")
     private String contact;
+
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email cannot be blank")
     private String email;
+
+    //TODO: get tha last purchase date with sale entity
+    @Null
+    private Date lastSaleDate;
 
 }
