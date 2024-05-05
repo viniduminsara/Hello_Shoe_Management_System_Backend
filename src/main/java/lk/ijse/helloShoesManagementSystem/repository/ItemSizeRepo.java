88cee52f0e81e7dfcec8d0af9e9dcedf94f826e5
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2024-05-05 21:06:56 +0530

    few changes

--- src/main/java/lk/ijse/helloShoesManagementSystem/repository/ItemSizeRepo.java
+++ src/main/java/lk/ijse/helloShoesManagementSystem/repository/ItemSizeRepo.java
@@ -2,6 +2,8 @@ package lk.ijse.helloShoesManagementSystem.repository;
 
 import lk.ijse.helloShoesManagementSystem.entity.ItemSizeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ItemSizeRepo extends JpaRepository<ItemSizeEntity, String> {
 }
--- src/main/java/lk/ijse/helloShoesManagementSystem/repository/SizeRepo.java
+++ src/main/java/lk/ijse/helloShoesManagementSystem/repository/SizeRepo.java
@@ -4,9 +4,11 @@ import lk.ijse.helloShoesManagementSystem.entity.SizeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface SizeRepo extends JpaRepository<SizeEntity, String> {
     @Query("SELECT s FROM SizeEntity s WHERE s.size = :size")
     Optional<SizeEntity> findBySize(@Param("size") Integer size);
