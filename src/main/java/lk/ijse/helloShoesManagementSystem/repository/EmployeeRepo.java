19ada333915a283433f6b5f6655a78191c4ac0fe
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2024-05-22 10:45:51 +0530

    few changes

--- src/main/java/lk/ijse/helloShoesManagementSystem/repository/EmployeeRepo.java
+++ src/main/java/lk/ijse/helloShoesManagementSystem/repository/EmployeeRepo.java
@@ -4,6 +4,9 @@ import lk.ijse.helloShoesManagementSystem.entity.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EmployeeRepo extends JpaRepository<EmployeeEntity, String> {
+    Optional<EmployeeEntity> findByEmail(String email);
 }
--- src/main/java/lk/ijse/helloShoesManagementSystem/repository/ItemSizeRepo.java
+++ src/main/java/lk/ijse/helloShoesManagementSystem/repository/ItemSizeRepo.java
@@ -10,7 +10,6 @@ import java.util.Optional;
 
 @Repository
 public interface ItemSizeRepo extends JpaRepository<ItemSizeEntity, String> {
-
     @Query(value = "SELECT * FROM item_size WHERE itemCode = :itemCode AND sizeId = :sizeId", nativeQuery = true)
     Optional<ItemSizeEntity> findByItemCodeAndSizeId(@Param("itemCode") String itemCode, @Param("sizeId") String sizeId);
 }
